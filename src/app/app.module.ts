ba0a436695c87cddc40ed7194271e0ba3470e8f5
9imeio <aengferreira@gmail.com>
2018-11-02 16:44:19 -0300

    Criando os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ServicesComponent,
+    GalleryComponent,
+    FooterComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule
